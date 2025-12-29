4e4f412e8645683ed01d517c78550f8079fb2d8d
goliakash <goliakash710@gmail.com>
2025-12-29 16:29:34 +0530

    small change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Automated CI/CD Pipeline 🚀"
+    print("Hello from Automated CI/CD Pipeline 🚀")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
